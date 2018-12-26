626be153886f8c32aa7d6d5d1f347fe953690df0
Adam Pettitt <adampettitt89@gmail.com>
2018-12-25 19:50:40 -0800

    dcm_subj_create.py creation

---
+++ org/conversion/dcm_subj_create.py
@@ -0,0 +1,30 @@
+# This script wil take a list made from the lcni directory
+# and will strip the excess leaving all the names
+import os
+
+# Set study info (may need to change for your study)
+# These variables are used only in this file for paths. Can omit if wanted.
+group = "adapt_lab"
+study = "SHARP"
+PI = "Allen"
+scriptsFolder = "SHARP_Scripts"
+
+# The following variables are used in the main script and need to be defined here. 
+# They need to exist prior to running the script
+
+# Directories
+dicomdir = os.path.join(os.sep, "projects", "lcni", "dcm", group, "Archive", "sharp")
+parentdir = os.path.join(os.sep, "projects", group, "shared", study) # folder that contains bidsdir and codedir
+codedir = os.path.join(parentdir, scriptsFolder, "org", "conversion") # Contains subject_list.txt, config file, and dcm2bids_batch.py
+
+subjectdir_contents = os.listdir(dicomdir)
+subjectdir_contents = filter(lambda k: len(k) < 16, subjectdir_contents)
+subjectdir_contents = list(filter(lambda k: 'SH' in k, subjectdir_contents))
+
+for subject in range(len(subjectdir_contents)):
+	subjectdir_contents[subject] = subjectdir_contents[subject][:5]
+
+subjectdir_contents = print('\n'.join(subjectdir_contents))
+
+with open(os.path.join(codedir,"subject_list.txt"), "w") as output:
+    output.write(str(subjectdir_contents))
