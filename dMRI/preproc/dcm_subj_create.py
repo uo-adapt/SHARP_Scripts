e9a9df23f2127fa6511e3197dbc0fe9135db4c1f
Adam Pettitt <adampettitt89@gmail.com>
2018-12-28 09:10:49 -0800

    added python script to make subject list

---
+++ dMRI/preproc/dcm_subj_create.py
@@ -0,0 +1,31 @@
+# This script wil take a list made from the lcni directory
+# and will strip the excess leaving all the names
+import os
+
+# Set study info (may need to change for your study)
+# These variables are used only in this file for paths. Can omit if wanted.
+group = "adapt_lab"
+study = "SHARP"
+PI = "Allen"
+scriptsFolder = "SHARP_Scripts"
+
+# The following variables are used in the main script and need to be defined here. 
+# They need to exist prior to running the script
+
+# Directories
+bidsdir = os.path.join()
+parentdir = os.path.join(os.sep, "projects", group, "shared", study) # folder that contains bidsdir and codedir
+bidsdir = os.path.join(parentdir,"bid_data")
+codedir = os.path.join(parentdir, scriptsFolder, "org", "conversion") # Contains subject_list.txt, config file, and dcm2bids_batch.py
+
+subjectdir_contents = os.listdir(bidsdir)
+subjectdir_contents = list(filter(lambda k: 'SH' in k, subjectdir_contents))
+
+
+with open(os.path.join(codedir,"subject_list.txt"), mode="w") as outfile:  # also, tried mode="rb"
+    for subject in subjectdir_contents:
+        outfile.write("%s\n" % subject)
+
+
+
+
