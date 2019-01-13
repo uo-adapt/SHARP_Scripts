e189a691807c9a1b010b88c065b44fdceeacf713
Adam Pettitt <pettitta@talapas-ln1.cm.cluster>
2019-01-12 17:35:36 -0800

    mc

--- rsfMRI/job_fmripreprest.sh
+++ rsfMRI/job_fmripreprest.sh
@@ -39,6 +39,7 @@ singularity run --bind "${group_dir}":"${group_dir}" ${image} ${bids_dir} ${deri
  -t ${task} --use-aroma --write-graph \
 --output-space {'T1w','template','fsaverage5','fsnative'} \
 --mem-mb 100000 \
+--skip_bids_validation \
 --fs-license-file $FS_LICENSE
 
 echo -e "\n"
