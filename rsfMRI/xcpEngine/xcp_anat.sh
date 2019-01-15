ea666c402613d7459a1bcec941ea8682e9e3cbfc
Adam Pettitt <adampettitt89@gmail.com>
2019-01-15 09:54:32 -0800

    mc

--- rsfMRI/xcpEngine/xcp_anat.sh
+++ rsfMRI/xcpEngine/xcp_anat.sh
@@ -7,7 +7,7 @@ HOME=/projects/adapt_lab/shared/SHARP/bids_data/derivatives/
 
 singularity run -B ${DATA_ROOT}:${HOME}xcpEngine  \
    /projects/adapt_lab/shared/containers/xcpEngine.simg
-   -d ${HOME}xcpEngine.simg/anat-antsct.dsn \
+   -d ${HOME}xcpEngine/anat-antsct.dsn \
    -c ${HOME}xcpEngine/anat_cohort.csv  \
    -o ${HOME}xcpEngine/xcp_output \
    -t 1 \
