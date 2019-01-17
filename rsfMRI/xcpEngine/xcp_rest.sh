66e7088e7cc15673a66b64e4d6bfed943210676b
Adam Pettitt <adampettitt89@gmail.com>
2019-01-17 12:48:07 -0800

    mc

--- rsfMRI/xcpEngine/xcp_rest.sh
+++ rsfMRI/xcpEngine/xcp_rest.sh
@@ -14,6 +14,4 @@ cd /projects/adapt_lab/shared/SHARP/SHARP_Scripts/rsfMRI/xcpEngine
 bash xcpEngine -d /projects/adapt_lab/shared/SHARP/SHARP_Scripts/rsfMRI/xcpEngine/fc-ICA-AROMA_201901170904.dsn \
 	-c /projects/adapt_lab/shared/SHARP/SHARP_Scripts/rsfMRI/xcpEngine/rest_cohort.csv \
 	-o /projects/adapt_lab/shared/SHARP/bids_data/derivatives/xcpEngine/data \
-	-t 1 \
-	-r /projects/adapt_lab/shared/SHARP/SHARP_Scripts/rsfMRI/xcpEngine
-
+	-t 1 
