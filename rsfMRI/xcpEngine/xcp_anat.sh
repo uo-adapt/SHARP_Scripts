168080fff09e375c2535f70ea5de02c1c533001a
Adam Pettitt <adampettitt89@gmail.com>
2019-01-22 10:09:53 -0800

    mc

--- rsfMRI/xcpEngine/xcp_anat.sh
+++ rsfMRI/xcpEngine/xcp_anat.sh
@@ -14,7 +14,7 @@ cd /projects/adapt_lab/shared/SHARP/SHARP_Scripts/rsfMRI/xcpEngine
 more "${TEMP_COHORT}",${ses}
 
 bash xcpEngine -d /projects/adapt_lab/shared/SHARP/SHARP_Scripts/rsfMRI/xcpEngine/anat-Complete+_201901151515.dsn \
-	-c echo "${TEMP_COHORT}",${ses} \
+	-c "${TEMP_COHORT}",${ses} \
 	-o /projects/adapt_lab/shared/SHARP/bids_data/derivatives/xcpEngine/data \
 	-t 2 \
 	-r /projects/adapt_lab/shared/SHARP/SHARP_Scripts/rsfMRI/xcpEngine \
