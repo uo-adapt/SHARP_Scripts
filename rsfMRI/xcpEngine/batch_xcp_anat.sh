7b894977702ebe02af8fb3d1f2d0505925d8cf91
Adam Pettitt <adampettitt89@gmail.com>
2019-01-21 21:53:41 -0800

    mc

--- rsfMRI/xcpEngine/batch_xcp_anat.sh
+++ rsfMRI/xcpEngine/batch_xcp_anat.sh
@@ -19,7 +19,7 @@ TEMP_COHORT=${FULL_COHORT}.${ID}.csv
 echo $HEADER > $TEMP_COHORT
 echo $LINE >> $TEMP_COHORT
 
-sbatch --export ALL,ID=${ID},TEMP_COHORT=${TEMP_COHORT} --job-name xcp_anat_"${ID}" --partition=short --time=02:00:00 --mem=10G -o "${group_dir}"/"${study}"/SHARP_Scripts/rsfMRI/xcpEngine/output/"${ID}"_xcp_anat_output.txt -e "${group_dir}"/"${study}"/SHARP_Scripts/rsfMRI/xcpEngine/output/"${ID}"_xcp_anat_error.txt xcp_anat.sh
+sbatch --export ALL,ID=${ID},TEMP_COHORT="${TEMP_COHORT}" --job-name xcp_anat_"${ID}" --partition=short --time=02:00:00 --mem=10G -o "${group_dir}"/"${study}"/SHARP_Scripts/rsfMRI/xcpEngine/output/"${ID}"_xcp_anat_output.txt -e "${group_dir}"/"${study}"/SHARP_Scripts/rsfMRI/xcpEngine/output/"${ID}"_xcp_anat_error.txt xcp_anat.sh
 
 
 
