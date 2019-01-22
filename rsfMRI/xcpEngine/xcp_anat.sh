92e6a282d93787d74199a74c95252f392e7affe4
Adam Pettitt <adampettitt89@gmail.com>
2019-01-22 10:03:27 -0800

    mc

--- rsfMRI/xcpEngine/xcp_anat.sh
+++ rsfMRI/xcpEngine/xcp_anat.sh
@@ -11,10 +11,10 @@ module load python3
 
 cd /projects/adapt_lab/shared/SHARP/SHARP_Scripts/rsfMRI/xcpEngine
 
-echo "${TEMP_COHORT}",${ses}
+more "${TEMP_COHORT}",${ses}
 
 bash xcpEngine -d /projects/adapt_lab/shared/SHARP/SHARP_Scripts/rsfMRI/xcpEngine/anat-Complete+_201901151515.dsn \
-	-c "${TEMP_COHORT}",${ses} \
+	-c echo "${TEMP_COHORT}",${ses} \
 	-o /projects/adapt_lab/shared/SHARP/bids_data/derivatives/xcpEngine/data \
 	-t 2 \
 	-r /projects/adapt_lab/shared/SHARP/SHARP_Scripts/rsfMRI/xcpEngine \
