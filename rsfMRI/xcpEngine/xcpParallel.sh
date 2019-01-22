be45b1162e028774bd14e6b422c11d69b9bbec11
Adam Pettitt <pettitta@talapas-ln1.cm.cluster>
2019-01-22 09:34:33 -0800

    mc

--- rsfMRI/xcpEngine/xcpParallel.sh
+++ rsfMRI/xcpEngine/xcpParallel.sh
@@ -2,11 +2,6 @@
 #$ -V
 #$ -t 1-3
 
-#SBATCH --partition=long        ### Partition (like a queue in PBS)
-#SBATCH --job-name=xcpAnat      ### Job Name
-#SBATCH --nodes=1               ### Number of nodes needed for the job
-#SBATCH --mem=100G
-
 module load singularity
 module load afni
 module load ants
@@ -21,12 +16,12 @@ SIMG=/projects/adapt_lab/shared/containers/xcpEngine.simg
 FULL_COHORT=/projects/adapt_lab/shared/SHARP/SHARP_Scripts/rsfMRI/xcpEngine/anat_cohort.csv
 
 # Create a temp cohort file with 1 line
-HEADER=$(head -n 1 $FULL_COHORT)
-LINE_NUM=$( expr $SGE_TASK_ID + 1 )
-LINE=$(awk "NR==$LINE_NUM" $FULL_COHORT)
-TEMP_COHORT=${FULL_COHORT}.${SGE_TASK_ID}.csv
-echo $HEADER > $TEMP_COHORT
-echo $LINE >> $TEMP_COHORT
+HEADER=id0,id1,img
+LINE_NUM=1
+LINE=
+TEMP_COHORT=/projects/adapt_lab/shared/SHARP/SHARP_Scripts/rsfMRI/xcpEngine/anat_cohort.csv..csv
+echo  > 
+echo  >> 
 
 cd /projects/adapt_lab/shared/SHARP/SHARP_Scripts/rsfMRI/xcpEngine
 
