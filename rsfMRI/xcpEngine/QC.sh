8be0d1628bc9af699f3d7e61d46a8616358a518d
Adam Pettitt <adampettitt89@gmail.com>
2019-04-21 11:29:53 -0700

    mc

--- rsfMRI/xcpEngine/QC.sh
+++ rsfMRI/xcpEngine/QC.sh
@@ -4,7 +4,7 @@ XCPEDIR=/projects/adapt_lab/shared/SHARP/SHARP_Scripts/rsfMRI/xcpEngine
 outputdir=/projects/adapt_lab/shared/data/BIDS_data/derivatives/xcpEngine/data
 
 ${XCPEDIR}/utils/combineOutput \
--p $outputdir  \  # all subjects directory  after running xcpEngine
--f "*quality.csv" \  # the extention of the file users want to combine
--o XCP_QAVARS.csv  # the output file in csv
+	-p $outputdir \
+	-f "*quality.csv" \
+	-o XCP_QAVARS.csv
 
