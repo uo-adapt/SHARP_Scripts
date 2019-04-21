94732897d3c4fda61b17ec3a0f97b2cdaff562ac
Adam Pettitt <adampettitt89@gmail.com>
2019-04-21 11:30:35 -0700

    mc

--- rsfMRI/xcpEngine/QC.sh
+++ rsfMRI/xcpEngine/QC.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 XCPEDIR=/projects/adapt_lab/shared/SHARP/SHARP_Scripts/rsfMRI/xcpEngine
-outputdir=/projects/adapt_lab/shared/data/BIDS_data/derivatives/xcpEngine/data
+outputdir=/projects/adapt_lab/shared/SHARP/bids_data/derivatives/xcpEngine/data
 
 ${XCPEDIR}/utils/combineOutput \
 	-p $outputdir \
