bb8d4f428c1db3f906310503aa67bd8cd366517e
Adam Pettitt <adampettitt89@gmail.com>
2019-04-25 17:25:36 -0700

    mc

--- rsfMRI/xcpEngine/fcon.sh
+++ rsfMRI/xcpEngine/fcon.sh
@@ -5,5 +5,5 @@ outputdir=/projects/adapt_lab/shared/SHARP/bids_data/derivatives/xcpEngine/data
 
 ${XCPEDIR}/utils/combineOutput \
 	-p $outputdir \
-	-f "ses-*/run-*/fcon/gordon333/*_ts.1D" \
+	-f "*_ts.1D" \
 	-o XCP_FCON.csv
